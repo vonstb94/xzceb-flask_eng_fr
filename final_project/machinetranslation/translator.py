f1b8a94ae7c6725871931acf1f9aef3302d5320d
vonstb94 <andy.vonstauffenberg@outlook.com>
2023-01-05 22:41:09 -0500

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,9 +1,8 @@
-''' Module providing Translator Services'''
-
-import json
+#import json
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,7 +10,6 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-# Prepare the Authenticator
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
@@ -20,24 +18,23 @@ language_translator = LanguageTranslatorV3(
 
 language_translator.set_service_url(url)
 
-# Translate English to French
 def english_to_french(english_text):
-    """ docstring. """
-    french_text = language_translator.translate(
-        text=english_text,
-        model_id='en-fr').get_result()
 
-    print(json.dumps(french_text, indent=2, ensure_ascii=False))
-
+    french_text = ''
+    if len(english_text) > 0:
+        translation = language_translator.translate(
+            text=english_text,
+            model_id='en-fr').get_result()
+        french_text = translation['translations'][0]['translation']
     return french_text
 
-# Translate French to English
 def french_to_english(french_text):
-    """ docstring. """
-    english_text = language_translator.translate(
-        text=french_text,
-        model_id='fr-en').get_result()
 
-    print(json.dumps(english_text, indent=2, ensure_ascii=False))
+    english_text = ''
 
+    if len(french_text) > 0:
+        translation = language_translator.translate(
+            text=french_text,
+            model_id='fr-en').get_result()
+        english_text = translation['translations'][0]['translation']
     return english_text
--- final_project/server.py
+++ final_project/server.py
@@ -1,30 +1,21 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
-import machinetranslation
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
-    text_translated_to_french = machinetranslation.translator.english_to_french(textToTranslate)
-
-    return text_translated_to_french
+    return translator.english_to_french(textToTranslate)
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
-    text_translated_to_english = machinetranslation.translator.french_to_english(textToTranslate)
-    return text_translated_to_english
+    return translator.french_to_english(textToTranslate)
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
     return render_template('index.html')
 
 if __name__ == "__main__":
